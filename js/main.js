15510ecfa87e7d27de19e975cce674d5b4ce4753
Alyne Abraham <52169769+alynea@users.noreply.github.com>
2019-12-10 22:14:02 -0600

    Updated CSS and media queries

--- js/main.js
+++ js/main.js
@@ -163,22 +163,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const restaurant_info = document.createElement('div');
+  restaurant_info.classList.add('restaurant-info');
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  restaurant_info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  restaurant_info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  restaurant_info.append(address);
+  li.append(restaurant_info);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+
+  const button = document.createElement('button');   
+  button.innerHTML = 'View Details'; 
+  button.addEventListener('click',() => {window.location.href = DBHelper.urlForRestaurant(restaurant); });
+  li.append(button);
 
   return li
 }
